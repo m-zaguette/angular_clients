fbeff482fa5a80e0fac1b890faaa28df2b5408cb
Marcelo Zaguette <Marcelo.Zaguette@fit-tecnologia.org.br>
2021-10-14 12:34:12 -0300

    Finalized initial design, pending update to dashboard version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './routing/app-routing.module';
+import { AppComponent } from './core/app-component/app.component';
+import { TemplateModule } from './core/template/template.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/template/template.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+  ]
+})
+export class TemplateModule { }
