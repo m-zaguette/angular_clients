493234002bcde7e2b8c94f0f0cb1d6398b287e2e
Marcelo Zaguette <Marcelo.Zaguette@fit-tecnologia.org.br>
2021-10-14 16:33:24 -0300

    Fixed errors and updated to sb-admin-2

--- src/app/core/app-component/app.component.ts
+++ src/app/core/app-component/app.component.ts
@@ -11,18 +11,58 @@ export class AppComponent implements AfterViewInit{
   title = 'angular-clients';
 
   ngAfterViewInit(): void{
+    // (function($) {
+    //   "use strict";
+    //     var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+    //       $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+    //           if (this.getAttribute("href") === path) {
+    //               $(this).addClass("active");
+    //           }
+    //       });   
+    //   $("#sidebarToggle").on("click", function(e) {
+    //     console.log("Toggle clicado");
+    //     e.preventDefault();
+    //     $("#accordionSidebar").toggleClass("toggled");
+    //   });
+    // })(jQuery);
+    
     (function($) {
-      "use strict";
-        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-              if (this.getAttribute("href") === path) {
-                  $(this).addClass("active");
-              }
-          });   
-      $("#sidebarToggle").on("click", function(e) {
-          e.preventDefault();
-          $("body").toggleClass("sb-sidenav-toggled");
+      "use strict"; // Start of use strict
+    
+      // Toggle the side navigation
+      $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+        if ($(".sidebar").hasClass("toggled")) {
+          (<any>$('.sidebar .collapse')).collapse('hide');
+        };
       });
-    })(jQuery);
+    
+      // Close any open menu accordions when window is resized below 768px
+      $(window).resize(function() {
+        if ((<any>$(window)).width() < 768) {
+          (<any>$('.sidebar .collapse')).collapse('hide');
+        };
+        
+        // Toggle the side navigation when window is resized below 480px
+        if (<any>($(window)).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+          $("body").addClass("sidebar-toggled");
+          $(".sidebar").addClass("toggled");
+          (<any>$('.sidebar .collapse')).collapse('hide');
+        };
+      });
+    
+      // Scroll to top button appear
+      $(document).on('scroll', function() {
+        var scrollDistance = (<any>$(this).scrollTop());
+        if (scrollDistance > 100) {
+          $('.scroll-to-top').fadeIn();
+        } else {
+          $('.scroll-to-top').fadeOut();
+        }
+      });
+        
+    })(jQuery); // End of use strict
+    
   }
 }
--- src/app/core/template/template.module.ts
+++ src/app/core/template/template.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
@@ -10,6 +12,8 @@ import { HomeComponent } from './home/home.component';
     NavbarComponent,
     SidebarComponent,
     HomeComponent,
+    FooterComponent,
+    ContentComponent,
   ],
   imports: [
     CommonModule
@@ -18,6 +22,8 @@ import { HomeComponent } from './home/home.component';
     NavbarComponent,
     SidebarComponent,
     HomeComponent,
+    FooterComponent,
+    ContentComponent,
   ]
 })
 export class TemplateModule { }
